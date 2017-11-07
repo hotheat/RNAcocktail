80dc93d06a5fc334e807073155dceba67f3d7b13
hotheat <guoam630@outlook.com>
2017-11-07 16:05:22 +0800

    remove_abspath

--- template.sh
+++
@@ -1,4 +0,0 @@
-export PATH=/hwfssz1/ST_OCEAN/USER/guojiao1/app/miniconda/bin/:$PATH
-source activate rnacock_2
-rm -rf ./RNA_pipeline/
-python3 /hwfssz1/ST_OCEAN/USER/guojiao1/Project/Pipeline/rnacocktail/template_rnacocktail.py
