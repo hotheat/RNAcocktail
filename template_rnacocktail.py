e8a1af2e441812741a68d251f800d9c6d2b6a5b4
hotheat <guoam630@outlook.com>
2017-11-07 09:58:20 +0800

    readme remove dictionary

--- template_rnacocktail.py
+++ template_rnacocktail.py
@@ -7,11 +7,6 @@ import time
 
 
 def log(*args, **kwargs):
-    """
-    用这个 log 替代 print
-    time.time() 返回 unix time
-    如何把 unix time 转化为人类能看得懂的格式
-    """
     format = '%Y/%m/%d %H:%M:%S'
     value = time.localtime(int(time.time()))
     dt = time.strftime(format, value)
